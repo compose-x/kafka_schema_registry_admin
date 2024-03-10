5f6eab2605c91ea8bd3ee6b424d083b15dd55c35
John "Preston" Mille <john@ews-network.net>
2024-03-10 16:17:25 +0000

    Better feature coverage and test coverage

--- kafka_schema_registry_admin/kafka_schema_registry_admin.py
+++ kafka_schema_registry_admin/kafka_schema_registry_admin.py
@@ -15,8 +15,7 @@ if TYPE_CHECKING:
 import json
 from enum import Enum
 from logging import getLogger
-
-import requests
+from urllib.parse import urlencode
 
 from .client_wrapper import Client
 from .client_wrapper.errors import NotFoundException
@@ -51,32 +50,59 @@ class SchemaRegistry:
         """
         return self.get_all_subjects().json()
 
-    def get_all_subjects(self) -> Response:
+    def get_all_subjects(
+        self, subject_prefix: str = None, deleted: bool = False
+    ) -> Response:
         """
         Method to get the list of subjects in the schema registry
         https://docs.confluent.io/platform/current/schema-registry/develop/api.html#get--subjects
 
         :raises: requests.exceptions.HTTPError
         """
-        return self.client.get("/subjects")
+        url_path: str = "/subjects"
+        if subject_prefix and deleted:
+            url_path += "?" + urlencode(
+                {"subjectPrefix": subject_prefix, "deleted": "true"}
+            )
+        elif subject_prefix:
+            url_path += f"?subjectPrefix={subject_prefix}"
+        elif deleted:
+            url_path += f"?deleted=true"
+        print(url_path)
+        return self.client.get(url_path)
 
     def get_subject_versions(self, subject_name: str) -> Response:
         """
         Method to get the list of subjects in the schema registry
         https://docs.confluent.io/platform/current/schema-registry/develop/api.html#get--subjects-(string-%20subject)-versions
-
-        :raises: requests.exceptions.HTTPError
         """
         return self.client.get(f"/subjects/{subject_name}/versions")
 
+    def get_subject_version_id(
+        self, subject_name: str, version_id: str | int = "latest"
+    ) -> Response:
+        """
+        Method to get the list of subjects in the schema registry
+        `API doc <https://docs.confluent.io/platform/current/schema-registry/develop/api.html#get--subjects-(string-%20subject)-versions-(versionId-%20version)>`__
+        """
+        url_path: str = f"/subjects/{subject_name}/versions/{version_id}"
+        LOG.debug(url_path)
+        return self.client.get(url_path)
+
+    def get_subject_version_id_schema(
+        self, subject_name: str, version_id: str | int = "latest"
+    ) -> Response:
+        """
+        Method to get the list of subjects in the schema registry
+        `API Doc <https://docs.confluent.io/platform/current/schema-registry/develop/api.html#get--subjects-(string-%20subject)-versions-(versionId-%20version)-schema>`__
+        """
+        url_path: str = f"/subjects/{subject_name}/versions/{version_id}/schema"
+        LOG.debug(url_path)
+        return self.client.get(url_path)
+
     def get_subject_versions_referencedby(self, subject_name, version_id) -> Response:
         """
-        https://docs.confluent.io/platform/current/schema-registry/develop/api.html
-        #get--subjects-(string-%20subject)-versions-versionId-%20version-referencedby
-
-        :param str subject_name:
-        :param int version_id:
-        :return: the request
+        `API Doc <https://docs.confluent.io/platform/current/schema-registry/develop/api.html#get--subjects-(string-%20subject)-versions-versionId-%20version-referencedby>`__
         """
         req = self.client.get(
             f"/subjects/{subject_name}/versions/{version_id}/referencedby"
@@ -86,7 +112,13 @@ class SchemaRegistry:
     def post_subject_schema(
         self, subject_name, definition, schema_type=None
     ) -> Response:
-        """Returns the schema ID and details if already exists, from the schema definition"""
+        """
+        Checks if the schema definition has already been registered with the subject.
+        Succeeds only if both the schema and the subject are registered. Returns 404 otherwise.
+        40401 - Subject not found
+        40403 - Schema not found
+        `API Doc <https://docs.confluent.io/platform/current/schema-registry/develop/api.html#post--subjects-(string-%20subject)>`__
+        """
         if isinstance(definition, dict):
             definition = str(json.dumps(definition))
         if schema_type is None:
@@ -101,8 +133,13 @@ class SchemaRegistry:
         return req
 
     def post_subject_schema_version(
-        self, subject_name, definition, schema_type=None
+        self, subject_name, definition, normalize: bool = False, schema_type=None
     ) -> Response:
+        """
+        `API Doc <https://docs.confluent.io/platform/current/schema-registry/develop/api.html#post--subjects-(string-%20subject)-versions>`__
+        Creates a new schema version for the given subject (and registers the subject if did not exist).
+        Returns the schema ID.
+        """
         try:
             return self.post_subject_schema(subject_name, definition, schema_type)
         except NotFoundException:
@@ -115,19 +152,18 @@ class SchemaRegistry:
 
             payload = {"schema": definition, "schemaType": schema_type}
             url = f"/subjects/{subject_name}/versions"
-            try:
-                req = self.client.post(url, json=payload)
-                return req
-            except Exception as error_create:
-                print("ERROR CREATE", error_create)
-                raise
+            if normalize:
+                url = f"{url}?normalize=true"
+            req = self.client.post(url, json=payload)
+            return req
 
     def delete_subject(
         self, subject_name, version_id=None, permanent=False
     ) -> Response:
         """
-        Method to delete a subject via its ID
+        Method to delete a subject entirely or a specific version
         https://docs.confluent.io/platform/current/schema-registry/develop/api.html#delete--subjects-(string-%20subject)
+        https://docs.confluent.io/platform/current/schema-registry/develop/api.html#post--subjects-(string-%20subject)-versions
 
         :param str subject_name:
         :param int version_id:
@@ -136,11 +172,17 @@ class SchemaRegistry:
         url = f"/subjects/{subject_name}"
         if version_id:
             url = f"{url}/versions/{version_id}"
-        req = self.client.delete(url)
+        try:
+            return self.client.delete(url)
+        except NotFoundException:
+            subjects = self.get_all_subjects(
+                subject_prefix=subject_name, deleted=True
+            ).json()
+            if subject_name in subjects and permanent:
+                return self.client.delete(f"{url}?permanent=true")
         if permanent:
             permanent_url = f"{url}?permanent=true"
-            req = self.client.delete(permanent_url)
-        return req
+            return self.client.delete(permanent_url)
 
     def get_schema_types(self) -> Response:
         """
@@ -164,15 +206,43 @@ class SchemaRegistry:
         req = self.client.get(url)
         return req
 
-    def post_compatibility_subjects_versions(
+    def post_compatibility_subject_versions(
+        self,
+        subject_name,
+        definition,
+        verbose: bool = False,
+        schema_type: str | Type = None,
+        references: list = None,
+    ) -> Response:
+        url = f"/compatibility/subjects/{subject_name}/versions"
+        return self.validate_subject_compatibility(
+            url, definition, schema_type, verbose=verbose, references=references
+        )
+
+    def post_compatibility_subject_version_id(
         self,
         subject_name,
         version_id,
         definition,
-        schema_type=None,
-        references=None,
+        verbose: bool = False,
+        schema_type: str | Type = None,
+        references: list = None,
     ) -> Response:
         url = f"/compatibility/subjects/{subject_name}/versions/{version_id}"
+        return self.validate_subject_compatibility(
+            url, definition, schema_type, verbose=verbose, references=references
+        )
+
+    def validate_subject_compatibility(
+        self,
+        url: str,
+        definition,
+        schema_type,
+        verbose: bool = False,
+        references: list = None,
+    ) -> Response:
+        if verbose:
+            url = f"{url}?verbose=true"
         LOG.debug(url)
         if isinstance(definition, dict):
             definition = str(json.dumps(definition))
